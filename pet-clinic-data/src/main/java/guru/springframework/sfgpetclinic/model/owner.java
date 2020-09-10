cbfed0b5d4804ac5357a20d5ff1c4d393c614ccf
golnoushte <golnoush.teimouri@gmail.com>
2020-09-10 16:53:58 +0300

    Add contact info to owner closes. #21

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/owner.java
@@ -3,7 +3,35 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class owner extends person {
+    private String address;
+    private String city;
+    private String phone;
+
     Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
