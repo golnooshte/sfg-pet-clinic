c944340e01967cd2a18c8bc943aa0bfa770ab590
golnoushte <golnoush.teimouri@gmail.com>
2020-09-10 16:45:09 +0300

    Create vet speciality entity , Associated to vet  closes. #20

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/vet.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class vet extends person{
+    Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
