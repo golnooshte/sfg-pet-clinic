cdb46464739b2c77d71fbb12f8ea751f734c2842
golnoushte <golnoush.teimouri@gmail.com>
2020-09-16 16:52:06 +0300

    Create SPringData JPA for PetTypeservice
     .closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpringDataJPA/VetsJPAService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpringDataJPA/VetsJPAService.java
@@ -3,12 +3,14 @@ package guru.springframework.sfgpetclinic.services.SpringDataJPA;
 import guru.springframework.sfgpetclinic.Repository.VetRepository;
 import guru.springframework.sfgpetclinic.model.vet;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 @Service
+@Profile("SpringDataJPA")
 public class VetsJPAService  implements VetService {
     private final VetRepository vetRepository;
 
