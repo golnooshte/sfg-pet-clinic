6000c718bc896b9a9f6854a390908634849156ce
golnoushte <golnoush.teimouri@gmail.com>
2020-09-16 13:44:48 +0300

    Create base Entity JPA .  closes #27

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/vetControlller.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/vetControlller.java
@@ -13,7 +13,7 @@ public class vetControlller {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    @RequestMapping({"/vets","/vets/index","/vets/index.html", "vets.html"})
     public String vetList(Model model){
 
         model.addAttribute("vets",vetService.findAll());
